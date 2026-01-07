48a20c8eafbeab44f0157cc71a928725d0c28d16
Joby-John <joby432john@gmail.com>
2026-01-07 15:58:51 +0530

    feat: Add react-router for navigation and update App component structure and for create multiple pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,16 @@
 import { HomePage } from './pages/HomePage'
+import {Routes, Route} from 'react-router'
+
 import './App.css'
 
 function App() {
 
   return (
-    <HomePage/>
+    <Routes>
+      {/* <Route path='/' element={<HomePage/>}></Route> */}
+      <Route index element={<HomePage/>}></Route>
+      <Route path='/checkout' element={<div>Test Check out page</div>}></Route>
+    </Routes>
   )
 }
 
